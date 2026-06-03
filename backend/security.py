c77bda883fe1b27984aaf409c16bd386252624d9
Priyanshu-aggarwal8 <agarwalpriyanshu812@gmail.com>
2026-06-03 14:42:45 +0530

    Added firebase config to env file

--- backend/security.py
+++ backend/security.py
@@ -6,9 +6,27 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from database import get_db
 import models
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials
 
-cred = credentials.Certificate("firebase-adminsdk.json")
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials
+
 if not firebase_admin._apps:
+
+    firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
+
+    if firebase_json:
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+
+    else:
+        cred = credentials.Certificate("firebase-adminsdk.json")
+
     firebase_admin.initialize_app(cred)
 
 security = HTTPBearer()
