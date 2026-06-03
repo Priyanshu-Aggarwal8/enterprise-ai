6a73d5ba71b625da67a5d7479242996751ae597e
Priyanshu-aggarwal8 <agarwalpriyanshu812@gmail.com>
2026-06-03 21:38:21 +0530

    Bug fix

--- backend/security.py
+++ backend/security.py
@@ -17,15 +17,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 if not firebase_admin._apps:
-
-    firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
-
-    if firebase_json:
-        cred_dict = json.loads(firebase_json)
-        cred = credentials.Certificate(cred_dict)
-
-    else:
-        cred = credentials.Certificate("firebase-adminsdk.json")
+    cred = credentials.Certificate("firebase-adminsdk.json")
 
     firebase_admin.initialize_app(cred)
 
